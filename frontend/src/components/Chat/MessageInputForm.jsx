5c594fa1c53cd847058f4b72291ae8641825ac0a
tanpreetjolly <tanpreet2002@gmail.com>
2024-02-04 22:23:03 +0530

    remove: file send option

--- frontend/src/components/Chat/MessageInputForm.jsx
+++ frontend/src/components/Chat/MessageInputForm.jsx
@@ -5,7 +5,6 @@ const MessageInputForm = ({
   newMessage,
   setNewMessage,
   sendMessage,
-  sendFile,
 }) => {
   return (
     <form onSubmit={sendMessage} className="relative  m-4 w-[85%]">
@@ -18,21 +17,6 @@ const MessageInputForm = ({
         onChange={(ev) => setNewMessage(ev.target.value)}
         required
       />
-      <label className="absolute top-0 end-9 bg-blue-200 p-2 text-gray-600 cursor-pointer rounded-sm border border-blue-200">
-        <input type="file" className="hidden" onChange={sendFile} />
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="currentColor"
-          className="w-6 aspect-square"
-        >
-          <path
-            fillRule="evenodd"
-            d="M18.97 3.659a2.25 2.25 0 00-3.182 0l-10.94 10.94a3.75 3.75 0 105.304 5.303l7.693-7.693a.75.75 0 011.06 1.06l-7.693 7.693a5.25 5.25 0 11-7.424-7.424l10.939-10.94a3.75 3.75 0 115.303 5.304L9.097 18.835l-.008.008-.007.007-.002.002-.003.002A2.25 2.25 0 015.91 15.66l7.81-7.81a.75.75 0 011.061 1.06l-7.81 7.81a.75.75 0 001.054 1.068L18.97 6.84a2.25 2.25 0 000-3.182z"
-            clipRule="evenodd"
-          />
-        </svg>
-      </label>
       <button
         type="submit"
         className="absolute top-0 end-0 px-1.5 h-full font-medium text-white bg-blue-700 rounded-e-lg "
