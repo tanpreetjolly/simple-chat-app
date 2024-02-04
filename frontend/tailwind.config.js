a75f13d2c257781f84fdc1d1488fb3dc4f10b3c1
tanpreetjolly <tanpreet2002@gmail.com>
2024-02-04 22:05:19 +0530

    add: color pallete added

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "#3D3B3B",
+        primary: "#72D2C2",
+        primarySecond: "#E3D35A",
+        secondary: "#30E0A1",
+        tertiary: "#EB7050",
+      },
+    },
   },
   plugins: [],
-}
+};
 
