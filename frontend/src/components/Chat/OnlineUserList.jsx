16f1ab45743b6a7ff2aa81838e246397afabc818
tanpreetjolly <tanpreet2002@gmail.com>
2024-02-08 13:28:00 +0530

    chore(ui) : search bar

--- frontend/src/components/Chat/OnlineUserList.jsx
+++ frontend/src/components/Chat/OnlineUserList.jsx
@@ -22,12 +22,28 @@ const OnlineUsersList = ({
 
   return (
     <section className="w-[30%] m-3">
-      <div className="text-black">
+      <div className="text-white flex items-center gap-2 p-3 bg-primary w-[90%] mx-auto rounded-2xl">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+          />
+        </svg>
+
         <input
           type="text"
           placeholder="Search..."
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
+          className="w-full bg-transparent outline-none"
         />
       </div>
       {filteredOnlinePeople.map((userId) => (
