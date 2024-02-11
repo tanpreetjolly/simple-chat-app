15e5a6b6f9e3ece5b87f2739aefb9941f3489d18
tanpreetjolly <tanpreet2002@gmail.com>
2024-02-11 15:48:25 +0530

    refactor:  avatar component styling

--- frontend/src/components/Chat/Avatar.jsx
+++ frontend/src/components/Chat/Avatar.jsx
@@ -1,25 +1,28 @@
 import "./Avatar.css";
 export default function Avatar({ username, userId, isOnline }) {
-  const colors = [
-    "teal",
-    "red",
-    "violet",
-    "purple",
-    "bg-indigo",
-    "orange",
-    "pink",
-    "green",
-    "yellow",
-    "blue",
-  ];
+  // const colors = [
+  //   "teal",
+  //   "red",
+  //   "violet",
+  //   "purple",
+  //   "bg-indigo",
+  //   "orange",
+  //   "pink",
+  //   "green",
+  //   "yellow",
+  //   "blue",
+  // ];
 
-  // console.log(username)
-  const userIdBase10 = parseInt(userId.substring(10), 16);
-  const colorIndex = userIdBase10 % colors.length;
-  const color = colors[colorIndex];
+
+  // const userIdBase10 = parseInt(userId.substring(10), 16);
+  // const colorIndex = userIdBase10 % colors.length;
+  // const color = colors[colorIndex];
   return (
     <div className={`squircle relative text-black`}>
-      <div className="squircle__inline">{username[0]}</div>
+    
+      <div className="squircle__inline text-xl text-white bg-primarySecond">
+        {username[0]}
+      </div>
       <div
         className={`absolute h-2 aspect-square rounded-full right-0 bottom-1 z-20 ${
           isOnline && "bg-green-500"
@@ -29,7 +32,7 @@ export default function Avatar({ username, userId, isOnline }) {
         {`
         .squircle {
           --squircle-fg: var(--bg, #ffffff);
-          --squircle-size: 40px;
+          --squircle-size: 44px;
           --squircle-radii: 50% / 10%;
         
           aspect-ratio: 1;
@@ -43,7 +46,7 @@ export default function Avatar({ username, userId, isOnline }) {
         .squircle::before,
         .squircle::after {
           align-self: center;
-          background-color: ${color}; 
+          background-color: #6B8AFD; 
           content: "";
           grid-column: 1;
           grid-row: 1;
