e77f347e6e623c70cbf9b6046704fc0bb98e4b8f
tanpreetjolly <tanpreet2002@gmail.com>
2024-02-12 16:16:43 +0530

    replace: nav logo

--- frontend/src/components/Chat/Nav.jsx
+++ frontend/src/components/Chat/Nav.jsx
@@ -4,26 +4,20 @@ import { Link } from "react-router-dom";
 const Nav = () => {
   return (
     <header className="w-[9%] text-white bg-dark flex flex-col px-3 py-4">
-      <div className="flex gap-1 items-center justify-center border-b pb-2 mb-4 border-gray-600">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-6 h-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 0 1 .865-.501 48.172 48.172 0 0 0 3.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z"
-          />
-        </svg>
-        <span className="font-semibold text-xl mr-2">ChatApp</span>
-      </div>
-      <nav className="h-full flex flex-col my-4 justify-between font-medium">
-        <div className="flex flex-col gap-5">
-          <Link to="/profile" className="flex items-end gap-0.5">
+      <Link
+        to="/"
+        className="flex gap-2 items-center justify-center border-b pb-2 mb-4 border-gray-600"
+      >
+        <img
+          src="https://flowbite.com/docs/images/logo.svg"
+          className="h-8"
+          alt="Flowbite Logo"
+        />
+        <span className="font-semibold text-xl mr-2">Swift</span>
+      </Link>
+      <nav className="h-full flex flex-col my-4 justify-between font-medium pl-2">
+        <div className="flex flex-col gap-5 ">
+          <Link to="/profile" className="flex items-end gap-1">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -40,7 +34,7 @@ const Nav = () => {
             </svg>
             <span>Profile</span>
           </Link>
-          <Link className="flex gap-0.5">
+          <Link to="/chathome" className="flex gap-1">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -58,7 +52,7 @@ const Nav = () => {
             <span>Chats</span>
           </Link>
         </div>
-        <Link className="flex items-end gap-0.5">
+        <Link className="flex items-end gap-1">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
