bdd75cc25bac87c9c078687e48f8a6064a065199
Vishal Rai <135126334+Vishalra-i@users.noreply.github.com>
2024-02-29 10:07:10 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -48,7 +48,7 @@ const Login = () => {
     }
   };
   return (
-    <section className="bg-dark md:h-screen">
+    <section className="bg-dark md:min-h-screen">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <div className="w-full  rounded-lg shadow border md:mt-0 sm:max-w-md xl:p-0 bg-gray-800 border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
