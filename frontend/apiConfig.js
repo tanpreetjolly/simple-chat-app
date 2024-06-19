12feada81697cdd3e82b5f141f9174c60de33d38
Tanpreet Singh Jolly <tanpreet2002@gmail.com>
2024-06-19 11:51:23 +0530

    Update apiConfig.js

--- frontend/apiConfig.js
+++ frontend/apiConfig.js
@@ -3,8 +3,8 @@ let baseUrl;
 let socketUrl;
 
 if (import.meta.env.VITE_NODE_ENV === "production") {
-  baseUrl = "https://simple-chat-app-pearl.vercel.app";
-  socketUrl = "wss://simple-chat-app-pearl.vercel.app";
+  baseUrl = "https://simple-chat-app-6yeb.onrender.com";
+  socketUrl = "wss://simple-chat-app-6yeb.onrender.com";
 } else {
   baseUrl = "http://localhost:4000";
   socketUrl = "ws://localhost:4000";
